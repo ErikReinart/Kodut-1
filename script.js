db9d49de716af350ff984ae7792f27b9f9937762
ErikReinart <erikreinart112@gmail.com>
2025-11-02 13:43:17 +0200

    true to false
    Changed USE_REMOTE to false and added dropdown functionality.

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function dropdownMenu() {
 }
 const LOCAL_JSON_URL = "example.json";
 const REMOTE_JSON_URL = "https://api.npoint.io/ceb07bdd51bbfee906db"; 
-const USE_REMOTE = true; 
+const USE_REMOTE = false; 
 // Date
 function formatDate(isoString) {
   const d = new Date(isoString);
@@ -94,4 +94,5 @@ async function renderFeed() {
 }
 
 renderFeed();
+
 
