5e877dc14146cb3ebc473dcd46d232a2ee4691cd
Erik Reinart <reinarte@ut.ee>
2025-11-23 13:11:20 +0200

    Vue postit

---
+++ postit-app/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <HeaderBar />
+  <router-view />
+  <FooterBar />
+</template>
+
+<script>
+import HeaderBar from './components/HeaderBar.vue';
+import FooterBar from './components/FooterBar.vue';
+
+export default {
+  components: { HeaderBar, FooterBar }
+};
+</script>
---
+++ postit-app/src/component/HeaderBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <header class="navbar">
+    <div class="nav-left">
+      <router-link to="/" id="home">Home</router-link>
+      <router-link to="/signup">Signup</router-link>
+    </div>
+
+    <div class="nav-right">
+      <img
+        class="profile"
+        @click="show = !show"
+        src="https://cdn-icons-png.flaticon.com/512/149/149071.png"
+      />
+
+      <div class="dropdown" v-if="show">
+        <p>John Doe</p>
+        <p>john.doe@ut.ee</p>
+        <p>logout</p>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  data() {
+    return { show: false };
+  }
+};
+</script>
