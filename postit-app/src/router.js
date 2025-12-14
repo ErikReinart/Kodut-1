e28d51bf416925eea5da51a4fb63c5ec7c0fa24c
ErikReinart <erikreinart112@gmail.com>
2025-12-14 12:19:29 +0200

    Add routing for other views and protection

--- postit-app/src/router.js
+++ postit-app/src/router.js
@@ -1,12 +1,32 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
 // Imports page
-import SignupView from './views/SignupView.vue';
+import LoginView from './views/LoginView.vue'
+import SignupView from './views/SignupView.vue'
+import HomeView from './views/HomeView.vue'
+import AddPostView from './views/AddPostView.vue'
+import PostView from './views/PostView.vue'
+import ContactView from './views/ContactView.vue'
 
 const routes = [
-  { path: '/signup', component: SignupView }
-];
+  { path: '/login', component: LoginView },
+  { path: '/signup', component: SignupView },
+  { path: '/contact', component: ContactView },
+
+  { path: '/', component: HomeView, meta: { requiresAuth: true } },
+  { path: '/add', component: AddPostView, meta: { requiresAuth: true } },
+  { path: '/post/:id', component: PostView, meta: { requiresAuth: true } }
+]
 
 export default createRouter({
   history: createWebHistory(),
-  routes});
+  routes})
+
+router.beforeEach((to) => {
+  const token = localStorage.getItem('token')
+  if (to.meta.requiresAuth && !token) {
+    return '/login'
+  }
+})
+
+export default router
