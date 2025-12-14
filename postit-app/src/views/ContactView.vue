37cfb65c730f320ee650c9cb08fe9736a44d5112
ErikReinart <erikreinart112@gmail.com>
2025-12-14 12:33:41 +0200

    Add contact information to ContactView component

--- postit-app/src/views/ContactView.vue
+++ postit-app/src/views/ContactView.vue
@@ -1,1 +1,9 @@
-
+<template>
+  <main class="center-box">
+    <div class="form">
+      <h2>Contact Us</h2>
+      <p>Email: support@postit.ee</p>
+      <p>Phone: +372 5555 5555</p>
+    </div>
+  </main>
+</template>
