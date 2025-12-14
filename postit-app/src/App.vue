9e53d047ae94c53887439fac315bab124c968b54
ErikReinart <erikreinart112@gmail.com>
2025-12-14 21:59:55 +0200

    Update App.vue

--- postit-app/src/App.vue
+++ postit-app/src/App.vue
@@ -9,22 +9,10 @@
 <script>
 import HeaderBar from './components/HeaderBar.vue';
 import FooterBar from './components/FooterBar.vue';
-import Posts from "./components/Posts.vue";
 
 export default {
 
-  components: { HeaderBar, Posts, FooterBar },
-
-  computed: {
-    showPosts() {
-      // hide posts on signup page (BAD fix!!)
-      return this.$route && this.$route.path !== '/signup';
-    }
-  },
-
-  created() {
-    this.$store.dispatch('loadPosts')
-  }
+  components: { HeaderBar, FooterBar },
 };
 
 </script>
